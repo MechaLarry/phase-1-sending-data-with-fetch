f4fe8b5c146bcc363f6c7098e46cb5825fcd94c2
MechaLarry <mecha.larry@gmail.com>
2024-07-16 15:48:30 +0300

    add index.js for the solution

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+// Define the submitData function
+function submitData(name, email) {
+    // Make a POST request to /users with name and email
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ name, email })
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Retrieve the new id value and append it to the DOM
+      document.body.innerHTML += `<p>${data.id}</p>`;
+      return data;
+    })
+    .catch(error => {
+      // Append the error message to the DOM
+      document.body.innerHTML += `<p>${error.message}</p>`;
+      return error;
+    });
+  }
+  
